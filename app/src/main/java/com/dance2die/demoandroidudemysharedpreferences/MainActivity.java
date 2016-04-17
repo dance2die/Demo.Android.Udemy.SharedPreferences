7a07ad389a4d5614b8181f0193d0bc94bf521448
dance2die <dontbothermewithspam@gmail.com>
2016-04-17 09:29:34 -0400

    Updated menu bar

--- app/src/main/java/com/dance2die/demoandroidudemysharedpreferences/MainActivity.java
+++ app/src/main/java/com/dance2die/demoandroidudemysharedpreferences/MainActivity.java
@@ -53,7 +53,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.add) {
+            Log.i("Action button tapped", "add");
             return true;
         }
 
