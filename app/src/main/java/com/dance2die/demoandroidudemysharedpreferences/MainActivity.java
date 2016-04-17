9ad999c06f78d7884c74c434c51dcf404a19682f
dance2die <dontbothermewithspam@gmail.com>
2016-04-17 09:24:54 -0400

    Able to save preference in the shared preference

--- app/src/main/java/com/dance2die/demoandroidudemysharedpreferences/MainActivity.java
+++ app/src/main/java/com/dance2die/demoandroidudemysharedpreferences/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences sharedPreferences = this.getSharedPreferences(
                 "com.dance2die.demoandroidudemysharedpreferences", Context.MODE_PRIVATE);
+//        sharedPreferences.edit().putString("userName", "dance2die").apply();
+
+        String userName = sharedPreferences.getString("userName", "");
+        Log.i("userName", userName);
     }
 
     @Override
